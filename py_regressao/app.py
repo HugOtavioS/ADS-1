11df1c1ac7efbcf3332d5e0bcc8adbd164bc2a9e
HugOtavioS <hugotavio2005@gmail.com>
2025-03-06 10:26:33 -0300

    Update Template
    UI melhorada

--- py_regressao/app.py
+++ py_regressao/app.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-base = pd.read_csv('C:/Users/hugot/Documents/Projetos/ADS-1/py_regressao/bets.csv')
+base = pd.read_csv('C:/Users/hugot/OneDrive/Documentos/Projetos/ADS-1/py_regressao/datas.csv')
 # print(np.array(base[""]))
 
 app = Flask(__name__)
 
 # Dados de treinamento
-X_train = np.array(base["home_team_goal"]).reshape(-1, 1)  # Variável de entrada (reshape para uma matriz 2D)
-y_train = np.array(base["away_team_goal"])  # Variável de saída
+X_train = np.array(base["max_positive_polarity"]).reshape(-1, 1)  # Variável de entrada (reshape para uma matriz 2D)
+y_train = np.array(base["shares"])  # Variável de saída
 
 # Criar e treinar o modelo de regressão linear
 model = LinearRegression()
